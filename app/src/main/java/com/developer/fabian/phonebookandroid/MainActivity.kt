d84db245c4d62b31ca973e4797f74cd24154941e
Fabián Varela Bonett <jfabian.303@gmail.com>
2018-08-27 16:21:43 -0500

    Create initial project

---
+++ app/src/main/java/com/developer/fabian/phonebookandroid/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.developer.fabian.phonebookandroid
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
